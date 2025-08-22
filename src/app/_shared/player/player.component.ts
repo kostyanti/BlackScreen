5752696d1959bc1b1402a1935c1ca7037e975736
Kostyanty <velushehakostya@gmail.com>
2025-08-22 21:37:02 +0300

    Fix player and add fullscreen

--- src/app/_shared/player/player.component.ts
+++ src/app/_shared/player/player.component.ts
@@ -12,4 +12,19 @@ import { SliderComponent } from '../slider/slider.component';
 export class PlayerComponent {
   currentPlayerValue = 0;
   currentSoundValue = 0;
+
+  toggleFullscreen() {
+    const player = document.querySelector('.player') as HTMLElement;
+
+    if (!document.fullscreenElement) {
+      if (player.requestFullscreen) { player.requestFullscreen(); } 
+      else if ((player as any).webkitRequestFullscreen) { (player as any).webkitRequestFullscreen(); } 
+      else if ((player as any).msRequestFullscreen) { (player as any).msRequestFullscreen(); }
+    } 
+    else {
+      if (document.exitFullscreen) { document.exitFullscreen(); } 
+      else if ((document as any).webkitExitFullscreen) { (document as any).webkitExitFullscreen(); } 
+      else if ((document as any).msExitFullscreen) { (document as any).msExitFullscreen(); }
+    }
+  }
 }
